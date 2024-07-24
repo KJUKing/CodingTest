a5e1d090b5f413e199655a43d0946adf6119abbc
KJUKing <kyungjurhdqn@gmail.com>
2024-07-24 16:46:52 +0900

    240724 스택 배열 알고리즘

--- src/stackAndQueue/StackTest02_VPS.java
+++ src/stackAndQueue/StackTest02_VPS.java
@@ -12,6 +12,7 @@ package stackAndQueue;
  여러분은 입력으로 주어진 괄호 문자열이 VPS 인지 아닌지를 판단해서 그 결과를 YES 와 NO 로 나타내어야 한다.
  */
 
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Stack;
 
@@ -24,9 +25,82 @@ public class StackTest02_VPS {
 
         for (int i = 0; i < T; i++) {
             String input = sc.nextLine();
+            boolean isVPS;
+            ArrayList<Character> list = new ArrayList<>();
+
+            for (int j = 0; j < input.length(); j++) {
+                list.add(input.charAt(j));
+            }
+            isVPS = searchVPS(list);
+            if (isVPS) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+
+    }
+
+    public static  boolean searchVPS(ArrayList<Character> list) {
+        Stack<Character> stack = new Stack<>();
+//        if (!(list.getFirst().equals(')')))
+            for (int j = 0; j < list.size(); j++) {
+                for (int k = j + 1; k < list.size(); k++) {
+                    if (list.get(j) == '(' && list.get(k) == ')') {
+                        list.set(j, '0');
+                        list.set(k, '0');
+                        break;
+                    }
+                }
+            }
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) == '(' || list.get(i) == ')') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+}
+
+/*
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        sc.nextLine();
+
+        for (int i = 0; i < T; i++) {
+            String input = sc.nextLine();
             Stack<Character> stack = new Stack<>();
             boolean isVPS = true;
+
+            for (char c : input.toCharArray()) {
+                if (c == '(') {
+                    stack.push(c);
+
+                } else {
+                    if (stack.isEmpty()) {
+                        isVPS = false;
+                        break;
+
+                    } else {
+                        if (c == ')') {
+                            stack.pop();
+
+                        }
+                    }
+                }
+
+            }
+
+            if (!stack.isEmpty()) {
+                isVPS = false;
+
+            }
+
+            System.out.println(isVPS ? "YES" : "NO");
         }
     }
 
-}
+ */
